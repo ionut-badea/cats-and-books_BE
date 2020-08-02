4068a1fc1f9e5379ed60c8a3da500bbf40a0dad4
Jack <ionut.badea@yazee.me>
2020-08-02 15:25:56 +0300

    settings

--- blog/settings.py
+++ blog/settings.py
@@ -101,7 +101,7 @@ GRAPHQL_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
